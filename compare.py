16a54862f28c899ca182b18f11264d912a2cbd23
Paul Berry <stereotype441@gmail.com>
2012-10-17 13:37:16 -0700

    Extract code from compare_opengl_extensions to compare.py

--- compare.py
+++ compare.py
@@ -4,3 +4,18 @@ def diff_keys(a_keys, b_keys, a_name, b_name, entities_name):
         print('{0} in {1} but not {2}:'.format(entities_name, a_name, b_name))
         for key in sorted(key_diff):
             print('  {0}'.format(key))
+
+
+def diff_functions_by_extension(a_map, b_map, a_name, b_name):
+    a_exts = set(a_map.keys())
+    b_exts = set(b_map.keys())
+    diff_keys(a_exts, b_exts, a_name, b_name, 'extensions')
+    diff_keys(b_exts, a_exts, b_name, a_name, 'extensions')
+    common_exts = a_exts & b_exts
+    for ext in sorted(common_exts):
+        a_funcs = set(a_map[ext])
+        b_funcs = set(b_map[ext])
+        diff_keys(a_funcs, b_funcs, a_name, b_name,
+                  '{0} functions'.format(ext))
+        diff_keys(b_funcs, a_funcs, b_name, a_name,
+                  '{0} functions'.format(ext))
--- compare_opengl_extensions.py
+++ compare_opengl_extensions.py
@@ -3,19 +3,6 @@ import extensions
 import opengl
 
 
-extensions_exts = set(extensions.FUNCTIONS_BY_EXTENSION.keys())
-opengl_exts = set(opengl.FUNCTIONS_BY_EXTENSION.keys())
-compare.diff_keys(
-    extensions_exts, opengl_exts, 'extensions', 'opengl', 'extensions')
-compare.diff_keys(
-    opengl_exts, extensions_exts, 'opengl', 'extensions', 'extensions')
-common_exts = extensions_exts & opengl_exts
-for ext in sorted(common_exts):
-    extension_funcs = set(extensions.FUNCTIONS_BY_EXTENSION[ext])
-    opengl_funcs = set(opengl.FUNCTIONS_BY_EXTENSION[ext])
-    compare.diff_keys(
-        extension_funcs, opengl_funcs, 'extension spec', 'opengl',
-        '{0} functions'.format(ext))
-    compare.diff_keys(
-        opengl_funcs, extension_funcs, 'opengl', 'extension spec',
-        '{0} functions'.format(ext))
+compare.diff_functions_by_extension(
+    extensions.FUNCTIONS_BY_EXTENSION, opengl.FUNCTIONS_BY_EXTENSION,
+    'extension specs', 'opengl')
