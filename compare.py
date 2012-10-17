d5f7020ee2ce4bd0ee5b51ec80168e213b3bec6b
Paul Berry <stereotype441@gmail.com>
2012-10-17 12:53:36 -0700

    Sort diff output

--- compare.py
+++ compare.py
@@ -2,5 +2,5 @@ def diff_keys(a_keys, b_keys, a_name, b_name, entities_name):
     key_diff = a_keys - b_keys
     if key_diff:
         print('{0} in {1} but not {2}:'.format(entities_name, a_name, b_name))
-        for key in key_diff:
+        for key in sorted(key_diff):
             print('  {0}'.format(key))
